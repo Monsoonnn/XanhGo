529c31313adbd872e9a4c8aedfbb0d7331324af1
Nguyen Quang Anh <quanganh598912@gmail.com>
2025-08-27 08:34:15 +0700

    Update HomeScreen, fonts, icon library

---
+++ src/components/BottomBar/styles.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    bottomNav: {
+        position: "absolute",
+        left: 0,
+        right: 0,
+        bottom: 25,
+        flexDirection: "row",
+        backgroundColor: "#fff",
+        paddingVertical: 12,
+        elevation: 8,
+        justifyContent: "space-around",
+        width: "90%",
+        marginHorizontal: "5%",
+        borderRadius: 24,
+    },
+    navItem: {
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+    },
+});
